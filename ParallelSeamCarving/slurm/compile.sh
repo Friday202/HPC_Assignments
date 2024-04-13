24f74bf9cb25f65e10bfea11e07fc6d951471aee
Jakob <jakob.petek@hotmail.com>
2024-04-13 22:22:21 +0200

    Vaja 2

--- ParallelSeamCarving/slurm/compile.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-g++ -std=c++17 -fopenmp seam_carving.cpp -o seam_carving.out
--- ziga/comp_run_mac.sh
+++
@@ -1,5 +0,0 @@
-
-# clang -v -Xclang -fopenmp -std=c++17 -L/opt/homebrew/opt/libomp/lib -I/opt/homebrew/opt/libomp/include -lomp Image.cpp Main.cpp -lc++ -o run1.out
-# ./run1.out /Images/720x480.png 200
-clang -v -Xclang -fopenmp -std=c++17 -L/opt/homebrew/opt/libomp/lib -I/opt/homebrew/opt/libomp/include -lomp Image_2d.cpp -lc++ -o run2.out
-./run2.out 
